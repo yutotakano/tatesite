81cfcb70d1b5572a5e651fdb84d2945c85475b9c
Yuto Takano <moa17stock@gmail.com>
2025-07-29 02:07:08 +0200

    Use global classes for vertical text, fix half-width numbers, add demo text

--- src/plugins/rehypeVerticalMarkdown.ts
+++ src/plugins/rehypeVerticalMarkdown.ts
@@ -1,6 +1,5 @@
 import type { RehypePlugin } from "@astrojs/markdown-remark";
 import { findAndReplace, type Find, type Replace } from "hast-util-find-and-replace";
-import styles from "../pages/index.module.css";
 
 interface Options {};
 
@@ -41,13 +40,17 @@ const findAndReplaceBoten: [Find, Replace] = [
 // Replace one to three-letter alphanumerics wrapped with japanese characters into
 // upright text
 const findAndReplaceUpright: [Find, Replace] = [
-  /(?<=[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}])([a-zA-Z0-9]{1,3})(?=[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}])/gu,
+  /(?<=[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}。、！？])([a-zA-Z0-9０-９]{1,3})(?=[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}。、！？])/gu,
   (match: string) => {
     return {
       type: "element",
       tagName: "span",
-      properties: { className: [styles.inlineUpright] },
-      children: [{ type: "text", value: match }]
+      properties: { className: "inlineUpright" },
+      children: [{
+        type: "text",
+        // Convert full-width numbers to half-width
+        value: match.replaceAll(/([０-９])/g, (m) => String.fromCharCode(m.charCodeAt(0) - 0xFEE0))
+      }]
     };
   }
 ];
@@ -58,7 +61,7 @@ const findAndReplaceExclamation: [Find, Replace] = [
     return {
       type: "element",
       tagName: "span",
-      properties: { className: [styles.inlineUpright] },
+      properties: { className: "inlineUpright" },
       children: [{ type: "text", value: "!?" }]
     };
   }
